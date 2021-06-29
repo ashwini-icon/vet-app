55fd7adbba0b597b98427fd225f58ee250ec20ff
ashwini.icom <ashwini.icon@gmail.com>
2021-06-29 10:24:46 +0530

    Added expandable content

--- src/app/Component/home/vet-content/contents-dialog/contents-dialog.component.ts
+++ src/app/Component/home/vet-content/contents-dialog/contents-dialog.component.ts
@@ -1,16 +1,24 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Content } from 'src/app/Models/Content';
-
+import { animate, state, style, transition, trigger } from '@angular/animations';
 @Component({
   selector: 'app-content-dialog',
   templateUrl: './contents-dialog.component.html',
-  styleUrls: ['./contents-dialog.component.css']
+  styleUrls: ['./contents-dialog.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class ContentsDialogComponent implements OnInit {
   dataSource : any = [];
-  displayedColumns: string[] = ['position', 'title', 'author' ];
-
+  columnsToDisplay: string[] = ['Position', 'Title', 'Author' ];
+  isExpansionDetailRow = (i: number, row: Object) => row.hasOwnProperty('detailRow');
+  expandedElement: any;
   constructor(
     public dialogRef: MatDialogRef<ContentsDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
@@ -26,14 +34,15 @@ export class ContentsDialogComponent implements OnInit {
   createDataSourceArray() {
     var source: any[] = []
     this.data.contents.forEach((content: Content, index: number) => {
-      source.push({content_id:content.content_id, position:index+1, title:content.title, author:content.author })
+      source.push({content_id:content.content_id, Position:index+1, Title:content.title, Author:content.author, content_data: content.content_data})
     });
+    const rows: any = [];
+    //source.forEach(element => rows.push(element, { detailRow: true, element }));
     this.dataSource = source;
-    console.log(source);
+    console.log(rows);
   }
 
   contentClicked(row:any) {
-    this.dialogRef.close();
   }
 
 }
